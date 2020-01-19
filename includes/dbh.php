4a7cc4b3129a9987d49fbf20a6e8f397b052a377
Tejas S <41792273+Tejasharish@users.noreply.github.com>
2020-01-19 10:27:44 +0530

    Add files via upload

---
+++ includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost:3307";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName );
